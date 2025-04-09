a09b895a6b8b25c0913188c28ad6613257c20554
sanjeevini2107 <dotragavissd@gmail.com>
2025-04-09 09:00:55 +0530

    this is changed

--- Addition.java
+++ Addition.java
@@ -2,6 +2,6 @@ public class Addition{
       public static void main(String[] args){
             int a=10;
             int b=20;
-         System.out.println("a+b="+ (a+b));
+         System.out.println("a-b="+ (a-b));
       }
 }
